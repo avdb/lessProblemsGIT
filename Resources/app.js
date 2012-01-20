04368d0890b08bba8eb2a105165018d5cb52e0c9
avdb <a_vandenbosch5@hotmail.com>
2012-01-20 11:52:40 +0100

    dropdown+labels

--- Resources/app.js
+++ Resources/app.js
@@ -18,34 +18,132 @@ var tab1 = Titanium.UI.createTab({
     window:win1
 });
 
-var label1 = Titanium.UI.createLabel({
+var label1_1 = Titanium.UI.createLabel({
 	color:'#999',
-	text:'I am Window 1',
+	text:'Plaats',
 	font:{fontSize:20,fontFamily:'Helvetica Neue'},
 	textAlign:'center',
-	width:'auto'
+	width:'auto',
+	top:10,
+	left:20
 });
-var picker = Ti.UI.createPicker();
 
-var data = [];
-data[0]=Ti.UI.createPickerRow({title:'Bananas',custom_item:'b'});
-data[1]=Ti.UI.createPickerRow({title:'Strawberries',custom_item:'s'});
-data[2]=Ti.UI.createPickerRow({title:'Mangos',custom_item:'m'});
-data[3]=Ti.UI.createPickerRow({title:'Grapes',custom_item:'g'});
+var label1_2 = Titanium.UI.createLabel({
+	color:'#999',
+	text:'Gebouw',
+	font:{fontSize:20,fontFamily:'Helvetica Neue'},
+	textAlign:'center',
+	width:'auto',
+	top:60,
+	left:20
+});
+
+var label1_3 = Titanium.UI.createLabel({
+	color:'#999',
+	text:'Verdiep',
+	font:{fontSize:20,fontFamily:'Helvetica Neue'},
+	textAlign:'center',
+	width:'auto',
+	top:110,
+	left:20
+});
+
+var picker1_1 = Ti.UI.createPicker({
+	top:10,
+	left:100
+});
+
+var data1_1 = [];
+data1_1[0]=Ti.UI.createPickerRow({title:'De vest',custom_item:'v'});
+data1_1[1]=Ti.UI.createPickerRow({title:'De ham',custom_item:'h'});
+data1_1[2]=Ti.UI.createPickerRow({title:'Kruidtuin',custom_item:'k'});
 
 // turn on the selection indicator (off by default)
-picker.selectionIndicator = true;
+picker1_1.selectionIndicator = true;
 
-picker.add(data);
+picker1_1.add(data1_1);
+
+picker1_1.setSelectedRow(0,0,true);
+
+var visible=false;
+picker1_1.addEventListener('change',function(e)
+{
+	if (visible)
+	{
+		picker1_2.hide();
+		label1_2.hide();
+		visible=false;
+	}
+	else
+	{
+		picker1_2.show();
+		label1_2.show();
+		visible=true;
+	}
+});
+
+
+var picker1_2 = Ti.UI.createPicker({
+	top:60,
+	left:100,
+	visible:false
+});
+
+var data1_2 = [];
+data1_2[0]=Ti.UI.createPickerRow({title:'Verdiep 1',custom_item:'v1'});
+data1_2[1]=Ti.UI.createPickerRow({title:'Verdiep 2',custom_item:'v2'});
+data1_2[2]=Ti.UI.createPickerRow({title:'Verdiep 3',custom_item:'v3'});
+
+// turn on the selection indicator (off by default)
+picker1_2.selectionIndicator = true;
+
+picker1_2.add(data1_2);
+
+picker1_2.setSelectedRow(0,0,true);
+
+var visible2=false;
+picker1_2.addEventListener('change',function(e)
+{
+	if (visible2)
+	{
+		picker1_3.hide();
+		label1_3.hide();
+		visible=false;
+	}
+	else
+	{
+		picker1_3.show();
+		label1_3.show();
+		visible=true;
+	}
+});
 
 
 
-picker.setSelectedRow(0,1,true);
+var picker1_3 = Ti.UI.createPicker({
+	top:110,
+	left:100
+});
+
+var data1_3 = [];
+data1_3[0]=Ti.UI.createPickerRow({title:'Lokaal 1',custom_item:'l1'});
+data1_3[1]=Ti.UI.createPickerRow({title:'Lokaal 2',custom_item:'l2'});
+data1_3[2]=Ti.UI.createPickerRow({title:'Lokaal 3',custom_item:'l3'});
+
+// turn on the selection indicator (off by default)
+picker1_3.selectionIndicator = true;
+
+picker1_3.add(data1_3);
+
+picker1_3.setSelectedRow(0,0,true);
 
 
-
-win1.add(label1);
-win1.add(picker);
+win1.add(label1_1);
+win1.add(label1_2);
+win1.add(label1_3);
+win1.add(picker1_1);
+win1.add(picker1_2);
+win1.add(picker1_3);
 
 
 //
